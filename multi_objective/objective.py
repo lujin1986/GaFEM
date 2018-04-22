6e0cea561a75d87c2d376c638ff39171f1c78d23
Jin Lu <jin.lu86@yahoo.com>
2018-04-22 22:36:03 +0200

    Delete objective.py

--- multi_objective/objective.py
+++
@@ -1,7 +0,0 @@
-def objective(name, phenotype):
-	from deap import benchmarks
-	f = benchmarks.dtlz2
-	from numpy import loadtxt
-	a = loadtxt('T%s_template.txt' % name)
-	result = f(a,2)
-	return result	
